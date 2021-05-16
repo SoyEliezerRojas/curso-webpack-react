efa963c2304044280c12f07feb11a98af02cc31f
Eliezer Rojas <rojaseliezer123uy@gmail.com>
2021-05-16 13:43:35 -0300

    initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    mode: 'development',
+
+    resolve: {
+        extensions:['.js','.css','.jsx']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            }
+          },
+          {
+            test: /\.html$/,
+            use: [
+              { loader: 'html-loader' }
+            ]
+          },
+          {
+            test: /\.s[ac]ss$/,
+            use: [
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          }
+        ]
+      },
+    plugins:[
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+    
+}
